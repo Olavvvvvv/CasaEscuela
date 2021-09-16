c46664869102c6584b01cd89a11a26f30756de89
bdiender <57986970+bdiender@users.noreply.github.com>
2021-09-16 23:33:01 +0200

    Update main.py

--- tablet_of_terror/main.py
+++ tablet_of_terror/main.py
@@ -1,6 +1,7 @@
 from datetime import datetime as dt
 import time
 import random
+import sys
 
 
 def wait(wtime, verbose=False):
@@ -8,13 +9,14 @@ def wait(wtime, verbose=False):
     :str wtime: time in hh:mm:ss format, do nothing until time is reached
     '''
     while True:
-        # get the current
+        # get the current time
         ctime = dt.now().strftime('%H:%M:%S')
+        # if the current time matches the waiting time
         if ctime == wtime:
-            break
+            break  # break the loop
         elif verbose:
             print(ctime)
-        time.sleep(1)
+        time.sleep(1)  # else wait one second
 
 
 def generate_alarm_times(n, rtime, btime='10:00:00', etime='03:00:00'):
@@ -30,32 +32,46 @@ def generate_alarm_times(n, rtime, btime='10:00:00', etime='03:00:00'):
     time_delta = etime - btime
     time_delta = time_delta.seconds
 
+    # if rest time and n are incompatible with the time delta
+    if n * rtime >= time_delta:
+        while n * rtime >= time_delta:
+            rtime -= 1  # lower rest time until it is compatible
+
     times = []
-    # TODO: test whether the amount of alarms with the rest time is even
-    #       possible with the given time frame
-    while len(times) < n:
-        new_time = random.randint(1, time_delta)
+    
+    while len(times) < n:  # while not enough times have been generated
+        new_time = random.randint(1, time_delta)  # generate a new time
+        # check whether no other time falls within one rtime unit of the new time
         ntime_range = {n for n in range(new_time - rtime, new_time + rtime)}
+        # if there is
         if len(set(times).intersection(ntime_range)) > 0:
-            continue
+            continue  # try again
         else:
-            times.append(new_time)
+            times.append(new_time)  # else save the new time
     return times
 
+
+def generate_prompt():
+    '''
+    Generate a prompt, probably have it sent to the tablet as well
+    '''
+    pass
+
+
 def check_times(alarm_times):
-    """
+    '''
     :list alarm_times: list of alarm times
     check every second whether an alarm should be sounded or not
-    """
-    c1 = 0
-    c2 = 1
-    while c1 < len(alarm_times):
-        if c2 in alarm_times:
-            # TODO: Fix the line below
-            print(f"drink at {dt.now().strftime('%H:%M:%S')}!")
-            c1 += 1
-        c2 += 1
-        time.sleep(1)
+    '''
+    c1 = 0  # counts the number of prompts
+    c2 = 0  # counts the number of seconds
+    while c1 < len(alarm_times):  # while not all prompts have happened
+        if c2 in alarm_times:  # if the current second is in alarm_times
+            generate_prompt()
+            c1 += 1  # update the number of prompts
+        c2 += 1  # update the seconds
+        time.sleep(1)  # wait another second
+
 
 def main(n, rtime, d=3, btime='10:00:00', etime='03:00:00'):
     '''
@@ -65,11 +81,19 @@ def main(n, rtime, d=3, btime='10:00:00', etime='03:00:00'):
     :str btime: time in hh:mm:ss format from when the thing starts
     :str btime: time in hh:mm:ss format until when the thing goes
     '''
-    for _ in range(d):
-        wait(btime)
+    for _ in range(d):  # repeat for each day
+        wait(btime)  # wait until the starting time
         print(f"[{dt.now().strftime('%H:%M:%S')}] Go!")
+        # generate new random times for that day:
         times = generate_alarm_times(n, rtime, btime=btime, etime=etime)
-        check_times(times)
+        check_times(times)  # and check them
 
 
-main(20, 1800)  # give twenty alarms with a minimum of 1800 seconds between each
+if __name__ == '__main__':
+    n = int(sys.argv[1])
+    rtime = int(sys.argv[2])
+    d = int(sys.argv[3])
+    btime = sys.argv[4]
+    etime = sys.argv[5]
+
+    main(n, rtime, d=d, btime=btime, etime=etime)
