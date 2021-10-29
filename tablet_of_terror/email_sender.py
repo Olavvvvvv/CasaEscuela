898a5e3fc9317e96de373c65df1d1215d8ead091
Olavvvvvv <o.a.j.pelzer@students.uu.nl>
2021-10-29 14:08:12 +0200

    nieuweste email

--- tablet_of_terror/email_sender.py
+++ tablet_of_terror/email_sender.py
@@ -1,7 +1,7 @@
 import smtplib, ssl
 
 
-def send_email(message='Enter message',gebruiker='dylano'):
+def send_email(opdracht='Enter message',gebruiker='dylano'):
     user = 'tot@dylano.me'
     password = 'Kankerenporno'
     smtp_adres = 'mail.antagonist.nl'    
@@ -14,10 +14,18 @@ def send_email(message='Enter message',gebruiker='dylano'):
     
     
     port = 465
-    reciever = 'olavpelzer@live.nl'
+    reciever = 'tabletofterror21@gmail.com'
 
     context = ssl.create_default_context()
 
+    
+    message = "\r\n".join([
+  "From: tabletofterror21@gmail.com",
+  "To: tabletofterror21@gmail.com",
+  "Subject: TABLET OF TERROR",
+  "",
+  opdracht
+  ])
 
     print('sending email...')
     with smtplib.SMTP_SSL(smtp_adres, port, context=context) as server:
@@ -25,4 +33,4 @@ def send_email(message='Enter message',gebruiker='dylano'):
         server.sendmail(user,reciever,message)
     print('email sent!')
 
-send_email()#,gebruiker='gmail')
+send_email(gebruiker='gmail')
