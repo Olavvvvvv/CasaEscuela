84ed55524d6a52e9c2a1bdeeabe9f309ab33022d
Bas Diender <dienderb@gmail.com>
2021-10-29 10:24:03 +0200

    fixed email_sender.py

--- tablet_of_terror/email_sender.py
+++ tablet_of_terror/email_sender.py
@@ -1,18 +1,19 @@
 import smtplib, ssl
+def send_email(message_to_send):
+    user = 'tabletofterror21@gmail.com'
+    password = 'casaescuela21'
+    port = 465
+    reciever = 'tot@dylano.me'
+    message_header = """/
+    subject: TABLET OF TERROR
+    
+    Ok gasten,\n
+    """
+    message = message_header + message_to_send
 
-user = 'tabletofterror21@gmail.com'
-password = 'casaescuela21'
-port = 465
-reciever = 'floriskools1@hotmail.com'
+    context = ssl.create_default_context()
 
-message = """\
-subject: TABLET OF TERROR
 
-Laat je likken
-"""
-context = ssl.create_default_context()
-
-for _ in range(5):
     print('sending email...')
     with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
         server.login(user,password)
